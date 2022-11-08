8128542add85cd3d182304efcfee0abf964ed1e3
pavelkpetkov <pavelpetkov@mail.bg>
2022-11-08 16:19:51 +0200

    small changes

--- src/components/Details.js
+++ src/components/Details.js
@@ -8,6 +8,7 @@ const Details = () => {
     const history = useHistory();
     const { user } = useContext(AuthContext);
     const [collection, setCollection] = useState({});
+    const [input, setInput] = useState(false);
     const { id } = useParams();
 
     useEffect(() => {
@@ -35,22 +36,39 @@ const Details = () => {
         </>
     );
 
-    const userButton = <Link className="button" to="#" onClick={viewOwnerButtonClick}>View owner's profile</Link>;
+    const sendMessageHandler = (e) => {
+        const message = e.target.value;
+        console.log(message);
+    };
+
+    const userButtons = <div>
+        <Link className="button" to="#" onClick={viewOwnerButtonClick}>View owner's profile</Link>
+        <button className="button" to="#" onClick={() => setInput(!input)}>Send message</button>
+    </div>
 
     return (
         <section className="Details">
             <h3>Title: {collection.title}</h3>
             <article>
-                { 
-                    collection.collectionImages ?  collection.collectionImages.map((x, index) => <span key={index}><img alt="CollectionImage" src={x}></img></span>) : null
+                {
+                    collection.collectionImages ? collection.collectionImages.map((x, index) => <span key={index}><img alt="CollectionImage" src={x}></img></span>) : null
                 }
             </article>
             <p>Description: {collection.description}</p>
 
             {user._id && (user._id === collection._ownerId
                 ? ownerButtons
-                : userButton
+                : userButtons
             )}
+            {
+                input && <input type="text"
+                    className="message"
+                    style={{ width: '100%', height: '200px'}}
+                    name="message"
+                    placeholder="Message"
+                    onBlur={sendMessageHandler} />
+            }
+
         </section>
     )
 }
